81220e9e407b40425c21d4dc143c7b725b6a138c
zichun-xu <zichun.xu@yale.edu>
2022-12-12 13:45:55 -0500

    =update test and action

--- tests/testthat/test-CSCORE.R
+++ tests/testthat/test-CSCORE.R
@@ -1,19 +1,7 @@
 test_that("input must be a Seurat object", {
-  library(Seurat)
   test_dat = readRDS("fixtures/mic_highly_expressed_500_AD_control")
   count = test_dat[[1]]
   genes = colnames(count)
   expect_error(CSCORE(count, genes))
-})
-
-test_that("sequencing depth is calculated correctly", {
-  library(Seurat)
-  test_dat = readRDS("fixtures/mic_highly_expressed_500_AD_control")
-  count = test_dat[[1]]
-  genes = colnames(count)
-  obj = CreateSeuratObject(t(count))
-  rho_1 = CSCORE(obj, genes)[[1]]
-  rho_2 = CSCORE(obj, genes, rowSums(count))[[1]]
-  expect_true(mean(rho_1 == rho_2, na.rm = T) == 1)
 })
 
