62e5dad6f83e27a1fcd6a8671697fe4f2e8ea574
Chang <c.su@yale.edu>
2022-12-17 17:46:18 -0500

    Polish documentations and notations in IRLS

--- R/CSCORE_IRLS.R
+++ R/CSCORE_IRLS.R
@@ -1,15 +1,37 @@
-#' Iteratively reweighted least squares procedure from CS-CORE for estimating and testing cell-type-specific co-expressions using single cell RNA sequencing data
+#' Iteratively reweighted least squares (IRLS) procedure in CS-CORE
+#'
+#' The iteratively reweighted least squares procedure in CS-CORE for estimating and testing cell-type-specific co-expressions using single cell RNA sequencing data.
+#' More details on this procedure can be found in the CS-CORE paper: <https://doi.org/10.1101/2022.12.13.520181>.
 #'
 #' @param X A n by p matrix of UMI counts, where n denotes the number of cells and p denotes the number of genes
 #' @param seq_depth A length n vector of sequencing depths
-#' @param covar_weight A character string. If specified to be "regularized", the weight estimates will be regularized across genes
 #'
-#' @return A list of three p by p matrices: co-expression estimates, p values and test statistics
+#' @return A list of three p by p matrices:
+#' \describe{
+#'   \item{est}{co-expression estimates}
+#'   \item{p_value}{p values}
+#'   \item{test_stat}{test statistics}
+#' }
 #' @export
 #'
 #' @examples
-#' # to be added
-CSCORE_IRLS <- function(X, seq_depth, covar_weight = "regularized"){
+#' ## Toy example:
+#' ## run CSCORE on a simulated independent gene pair
+#' cscore_example <- CSCORE_IRLS(ind_gene_pair$counts, ind_gene_pair$seq_depths)
+#'
+#' ## Estimated co-expression between two genes
+#' cscore_example$est[1,2]
+#' # close to 0: 0.007820124
+#'
+#' ## p-values
+#' cscore_example$p_value[1,2]
+#' # not significant: 0.961981
+#'
+#' @source
+#' Cell-type-specific co-expression inference from single cell RNA-sequencing data
+#' Chang Su, Zichun Xu, Xinning Shan, Biao Cai, Hongyu Zhao, Jingfei Zhang;
+#' bioRxiv 2022.12.13.520181; doi: <https://doi.org/10.1101/2022.12.13.520181>
+CSCORE_IRLS <- function(X, seq_depth){
   if (is.null(seq_depth)) {
     seq_depth = apply(X, 1, sum, na.rm = T)
   }
@@ -18,45 +40,48 @@ CSCORE_IRLS <- function(X, seq_depth, covar_weight = "regularized"){
   seq_depth_sq = seq_depth^2
   seq_2 = sum(seq_depth_sq)
   seq_4 = sum(seq_depth^4)
-  mu = colSums(X*seq_depth)/seq_2
+  mu = colSums(X * seq_depth)/seq_2
   M = outer(seq_depth, mu)
   X_centered = X - M
   sigma2 = colSums(((X_centered^2 - M) * seq_depth_sq))/seq_4
   theta = mu^2/sigma2
   j = 0
-  error = Inf
+  delta = Inf
 
-  while( error > 0.05 & j <= 10 ){
+  while( delta > 0.05 & j <= 10 ){
     theta_previous = theta
     theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
     theta[theta < 0] = Inf
-    w1 = M + outer(seq_depth_sq, mu^2/theta_median)
-    w1[is.na(w1)|w1 <= 0] = 1
-    mu = colSums((X/w1)*seq_depth)/colSums(seq_depth_sq/w1)
+    w = M + outer(seq_depth_sq, mu^2/theta_median)
+    w[is.na(w)|w <= 0] = 1
+    mu = colSums((X/w) * seq_depth)/colSums(seq_depth_sq/w)
     M = outer(seq_depth, mu)
     X_centered = X - M
-    w2 = (M^2/theta_median + M)^2
-    w2[w2 <= 0] = 1
-    sigma2 = colSums(((X_centered^2 - M)/w2 * seq_depth_sq))/colSums(seq_depth_sq^2/w2)
+    h = (M^2/theta_median + M)^2
+    h[h <= 0] = 1
+    sigma2 = colSums(((X_centered^2 - M)/h * seq_depth_sq))/colSums(seq_depth_sq^2/h)
     theta = mu^2/sigma2
     j = j+1
-    print(paste0("Iteration: ", j, ", Median: ", theta_median))
-    error = max(abs(log((theta/theta_previous)[theta > 0 & theta_previous > 0])), na.rm = T)
-    print(error)
+    # print(paste0("Iteration: ", j, ", Median: ", theta_median))
+    delta = max(abs(log((theta/theta_previous)[theta > 0 & theta_previous > 0])), na.rm = T)
+    # print(delta)
+  }
+  if(j == 10 & delta > 0.05){
+    print('IRLS failed to converge after 10 iterations. Please check your data.')
+  }else{
+    print(sprintf('IRLS converged after %i iterations', j))
   }
 
   theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
   theta[theta < 0] = Inf
-  if( covar_weight == "regularized" ){
-    w1 = M + outer(seq_depth_sq, mu^2/theta_median)
-  } else{
-    w1 = M + outer(seq_depth_sq, mu^2/theta)
-  }
-  w1[is.na(w1)|w1 <= 0] = 1
+  w = M + outer(seq_depth_sq, mu^2/theta_median)
+  w[is.na(w)|w <= 0] = 1
 
   covar = matrix(NA, nrow = n_gene, ncol = n_gene)
-  covar <- (t(seq_depth_sq * X_centered/w1) %*% (X_centered/w1))/(t(seq_depth_sq/w1) %*% (seq_depth_sq/w1))
+  covar <- (t(seq_depth_sq * X_centered/w) %*% (X_centered/w))/(t(seq_depth_sq/w) %*% (seq_depth_sq/w))
 
+  # This step might generate NA, as sigma2 estimate from least squares can be negative.
+  # We will implement post-processing in CSCORE.R to address this.
   sigma <- sqrt(sigma2)
   est <- covar/outer(sigma, sigma)
   diag(est)[!is.na(diag(est))] <- 1
