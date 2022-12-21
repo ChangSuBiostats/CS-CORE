93bb66ee2fc2285f133e0e5d71292e934774d0cc
Chang <c.su@yale.edu>
2022-12-20 23:00:39 -0500

    Add a vignette

--- R/data.R
+++ R/data.R
@@ -10,3 +10,14 @@
 #'   \item{seq_depths}{A length 1972 vector of sequencing depths}
 #' }
 "ind_gene_pair"
+
+#' Top GO enrichment result in the CS-CORE vignette
+#'
+#' This data set stores the top GO enrichment result from the analysis in CS-CORE vignette.
+#' It is saved for efficiently rendering the vignette without re-evaluating the codes.
+#'
+#' @format ## `top_enrich_go`
+#' A list with five elements, where each element holds the top 3 GO terms enriched in a gene module with strong enrichment signals
+#'
+"top_enrich_go"
+
