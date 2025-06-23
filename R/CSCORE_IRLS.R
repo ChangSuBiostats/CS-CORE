0a112417b57694f15432c741775a610f04c57ab4
ChangSuBiostats <chang.su@emory.edu>
2025-06-23 13:29:58 -0500

    Features: support covariate adjustments; move base R implementation to _base

--- R/CSCORE_IRLS.R
+++ R/CSCORE_IRLS.R
@@ -1,17 +1,67 @@
 #' Iteratively reweighted least squares (IRLS) procedure in CS-CORE
 #'
-#' The iteratively reweighted least squares procedure in CS-CORE for estimating and testing cell-type-specific co-expressions using single cell RNA sequencing data.
-#' More details on this procedure can be found in the CS-CORE paper: <https://doi.org/10.1101/2022.12.13.520181>.
+#' This function implements the IRLS procedure used in CS-CORE for estimating and testing
+#' cell-type-specific co-expression from single-cell RNA sequencing data.
 #'
-#' @param X A n by p matrix of UMI counts, where n denotes the number of cells and p denotes the number of genes
-#' @param seq_depth A length n vector of sequencing depths
-#' @param post_process Whether to process the estimated co-expressions such that the estimates are between -1 and 1. Default to TRUE.
+#' Let \eqn{x_{ij}} denote the UMI count of gene \eqn{j} in cell \eqn{i};
+#' \eqn{s_i} denote the sequencing depth;
+#' \eqn{\mu_j,\sigma_{jj}, \sigma_{jj'}} denote the mean, variance and covariance;
+#' \eqn{c_{ik}} denote additional covariate \eqn{k} for cell \eqn{i} (e.g. disease status or cellular state).
+#' The procedure consists of two main steps:
+#' \enumerate{
+#'   \item \strong{Mean and variance estimation:} Estimate gene-specific mean and variance parameters
+#'         using two moment-based regressions:
+#'     \itemize{
+#'       \item \strong{Mean model:} \eqn{x_{ij} = s_i (\mu_j + \sum_k c_{ik} \beta_k) + \epsilon_{ij}}
+#'       \item \strong{Variance model:} \eqn{(x_{ij} - s_i \mu_{ij})^2 = s_i \mu_{ij} + s_i^2 (\sigma_{jj} + \sum_k c_{ik} \gamma_k) + \eta_{ij}},
+#'       where \eqn{\mu_{ij} = \mu_j + \sum_k c_{ik} \beta_k}
+#'     }
 #'
-#' @return A list of three p by p matrices:
+#'   \item \strong{Covariance estimation and hypothesis testing:} Estimate gene-gene covariance
+#'         and compute test statistics to assess the statistical significance of gene co-expression using a third moment-based regression:
+#'     \itemize{
+#'       \item \strong{Covariance model:} \eqn{(x_{ij} - s_i \mu_{ij})(x_{ij'} - s_i \mu_{ij'}) = s_i^2 (\sigma_{jj'} + \sum_k c_{ik} \theta_k) + \xi_{ijj'}}
+#'     }
+#' }
+#' We note that
+#' \enumerate{
+#'   \item The formulation above assumes that the covariates alter the mean / variance / covariance of underlying gene expression,
+#'   rather than observed counts.
+#'   \item The original CS-CORE published in <https://doi.org/10.1038/s41467-023-40503-7>
+#'   did not consider adjusting for covariates \eqn{c_{ik}}'s. This is equivalent to setting \code{covariates} to \code{NULL}.
+#' }
+#' @source Su, C., Xu, Z., Shan, X., Cai, B., Zhao, H., & Zhang, J. (2023).
+#' Cell-type-specific co-expression inference from single cell RNA-sequencing data.
+#' \emph{Nature Communications}.
+#' doi: <https://doi.org/10.1038/s41467-023-40503-7>
+#'
+#' @param X A numeric matrix of UMI counts (\code{n x p}),
+#'   where \code{n} is the number of cells and \code{p} is the number of genes.
+#' @param seq_depth A numeric vector of sequencing depths of length \code{n}.
+#' @param covariates A numeric matrix of covariates (\code{n x K}) to be adjusted for in the moment-based regressions.
+#'   Can be a length n vector if \eqn{K=1}.
+#'   If \code{NULL}, no covariates are adjusted for in the regression.
+#'   Defaults to \code{NULL}.
+#' @param post_process Logical; whether to rescale the estimated co-expressions to lie between –1 and 1.
+#'   Defaults to \code{TRUE}.
+#' @param covariate_level A character string indicating whether covariates are assumed to affect
+#'   the underlying gene expression levels (\code{"z"}) or the observed counts (\code{"x"}).
+#'   See the *Details* section for further explanation.
+#'   Defaults to \code{"z"}.
+#' @param adjust_setting A named logical vector of length 3;
+#'   whether to adjust for covariates at the mean, variance, and covariance level.
+#'   Defaults to \code{c(mean = T,var = T, covar = T)}.
+#' @param return_all Logical; whether to return all estimates, including the effect sizes for covariates.
+#'   Defaults to \code{FALSE}.
+#'
+#' @return A list containing the following components:
 #' \describe{
-#'   \item{est}{co-expression estimates}
-#'   \item{p_value}{p values}
-#'   \item{test_stat}{test statistics}
+#'   \item{est}{A \eqn{p \times p} matrix of co-expression estimates.}
+#'   \item{p_value}{A \eqn{p \times p} matrix of p-values.}
+#'   \item{test_stat}{A \eqn{p \times p} matrix of test statistics evaluating the statistical significance of co-expression.}
+#'   \item{mu_beta}{A \eqn{k \times p} matrix of regression coefficients from the mean model. Returned if \code{return_all = TRUE}.}
+#'   \item{sigma2_beta}{A \eqn{k \times p} matrix of regression coefficients from the variance model. Returned if \code{return_all = TRUE}.}
+#'   \item{cov_beta}{A \eqn{k \times p \times p} array of regression coefficients from the covariance model. Returned if \code{return_all = TRUE}.}
 #' }
 #' @export
 #'
@@ -28,47 +78,73 @@
 #' cscore_example$p_value[1,2]
 #' # not significant: 0.961981
 #'
-#' @source
-#' Cell-type-specific co-expression inference from single cell RNA-sequencing data
-#' Chang Su, Zichun Xu, Xinning Shan, Biao Cai, Hongyu Zhao, Jingfei Zhang;
-#' bioRxiv 2022.12.13.520181; doi: <https://doi.org/10.1101/2022.12.13.520181>
-CSCORE_IRLS <- function(X, seq_depth, post_process = TRUE){
-  if (is.null(seq_depth)) {
+
+CSCORE_IRLS <- function(X, seq_depth,
+                        covariates = NULL,
+                        post_process = TRUE,
+                        covariate_level = 'z',
+                        adjust_setting = c('mean' = T, 'var' = T, 'covar' = T),
+                        return_all = FALSE){
+  if(is.null(seq_depth)){
     seq_depth = apply(X, 1, sum, na.rm = T)
   }
   if(nrow(X) != length(seq_depth)){
     stop('The length of the sequencing depth must match the number of cells.')
+  }
+  if(has_non_integer(X)){
+    stop("CS-CORE takes the UMI count matrix as input. The input matrix X contains non-integer values.")
+  }
+  if(!is.null(covariates)){
+    if(is.vector(covariates)){
+      if(nrow(X) != length(covariates)) stop('The length of the covariate vector should match the number of cells.')
+    }else{
+      if(nrow(X) != nrow(covariates)) stop('The number of rows in the covariate matrix should match the number of cells.')
+      message(sprintf('Regression will adjust for %i covariates', ncol(covariates)))
+    }
   }
   n_cell = nrow(X)
   n_gene = ncol(X)
   seq_depth_sq = seq_depth^2
   seq_2 = sum(seq_depth_sq)
   seq_4 = sum(seq_depth^4)
-  mu = colSums(X * seq_depth)/seq_2
-  M = outer(seq_depth, mu)
+  # Construct design matrix
+  if(is.null(covariates)){
+    D <- matrix(rep(1, n_cell), ncol = 1)
+  }else{
+    D <- cbind(1, covariates)
+  }
+  D_mu <- set_D(seq_depth, D, adjust_setting['mean'], covariate_level)
+  D_sigma2 <- set_D(seq_depth_sq, D, adjust_setting['var'], covariate_level)
+  D_sigma <- set_D(seq_depth_sq, D, adjust_setting['covar'], covariate_level)
+  # initialize with OLS
+  mu_beta <- WLS_mean(D_mu, X, matrix(1, nrow = n_cell, ncol = n_gene))
+  M <- D_mu %*% mu_beta
+  mu <- mu_beta[1,]
   X_centered = X - M
-  sigma2 = colSums(((X_centered^2 - M) * seq_depth_sq))/seq_4
+  sigma2_beta <- WLS_mean(D_sigma2, X_centered^2 - M, matrix(1, nrow = n_cell, ncol = n_gene))
+  sigma2 <- sigma2_beta[1,]
   theta = mu^2/sigma2
   j = 0
   delta = Inf
 
+  # IRLS for estimating mean and variance parameters
   while( delta > 0.05 & j <= 10 ){
     theta_previous = theta
     theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
     theta[theta < 0] = Inf
     w = M + outer(seq_depth_sq, mu^2/theta_median)
     w[is.na(w)|w <= 0] = 1
-    mu = colSums((X/w) * seq_depth)/colSums(seq_depth_sq/w)
-    M = outer(seq_depth, mu)
+    mu_beta <- WLS_mean(D_mu, X, 1/w)
+    mu <- mu_beta[1,]
+    M <- D_mu %*% mu_beta
     X_centered = X - M
     h = (M^2/theta_median + M)^2
     h[h <= 0] = 1
-    sigma2 = colSums(((X_centered^2 - M)/h * seq_depth_sq))/colSums(seq_depth_sq^2/h)
+    sigma2_beta <- WLS_mean(D_sigma2, X_centered^2 - M, 1/h)
+    sigma2 <- sigma2_beta[1,]
     theta = mu^2/sigma2
     j = j+1
-    # print(paste0("Iteration: ", j, ", Median: ", theta_median))
     delta = max(abs(log((theta/theta_previous)[theta > 0 & theta_previous > 0])), na.rm = T)
-    # print(delta)
   }
   if(j == 10 & delta > 0.05){
     print('IRLS failed to converge after 10 iterations. Please check your data.')
@@ -76,32 +152,39 @@ CSCORE_IRLS <- function(X, seq_depth, post_process = TRUE){
     print(sprintf('IRLS converged after %i iterations.', j))
   }
 
+  # Update the weights for estimating covariance
   theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
   theta[theta < 0] = Inf
   w = M + outer(seq_depth_sq, mu^2/theta_median)
   w[is.na(w)|w <= 0] = 1
 
-  covar = matrix(NA, nrow = n_gene, ncol = n_gene)
-  covar <- (t(seq_depth_sq * X_centered/w) %*% (X_centered/w))/(t(seq_depth_sq/w) %*% (seq_depth_sq/w))
-
+  X_centered <- X - M
+  covar <- WLS_cov(D_sigma, X_centered, 1/w)$cov_hat[1,,]
   # Evaluate test statistics and p values
   Sigma <- M + outer(seq_depth_sq, sigma2)
   ele_inv_Sigma <- 1/Sigma
-  X_centered_scaled <- X_centered * ele_inv_Sigma
-  num <- t(seq_depth_sq * X_centered_scaled) %*% X_centered_scaled
-  deno <- sqrt(t(seq_depth^4 * ele_inv_Sigma) %*% ele_inv_Sigma)
-  test_stat <- num/deno
-  p_value <- 2 * stats::pnorm(abs(test_stat), lower.tail = F)
+  ts_res <- WLS_cov(D_sigma, X_centered, ele_inv_Sigma)
+  est <- ts_res$cov_hat[1,,]
+  p_value <- 2 * stats::pnorm(abs(ts_res$test_stat), lower.tail = F)
 
   # Evaluate co-expression estimates
   neg_gene_inds <- which(sigma2 < 0)
   sigma2[neg_gene_inds] <- 0
   sigma <- sqrt(sigma2)
   est <- covar/outer(sigma, sigma)
+  test_stat <- ts_res$test_stat
 
   # Post-process the co-expression estimates
+  diag(est) <- 1
   if(post_process) est <- post_process_est(est)
-  return(list(est = est, p_value = p_value, test_stat = test_stat))
+  rownames(est) <- colnames(est) <- rownames(p_value) <- colnames(p_value) <- rownames(test_stat) <- colnames(test_stat) <- colnames(X)
+  result_list <- list(est = est, p_value = p_value, test_stat = test_stat)
+  if(return_all){
+    result_list$mu_beta <- mu_beta
+    result_list$sigma2_beta <- sigma2_beta
+    result_list$cov_beta <- ts_res$cov_hat
+  }
+  return(result_list)
 }
 
 
@@ -142,4 +225,44 @@ post_process_est <- function(est){
   est[est > 1] <- 1
   est[est < -1] <- -1
   return(est)
+}
+
+#' Set the design matrix for moment-based regressions
+#'
+#'
+#' @param s A numeric vector of sequencing depths (for mean regression) or squared sequencing depths (for variance and covariance)
+#' @param D A numeric matrix of intercept and covariates (\code{n x K})
+#' @param adjust_setting Logical; whether to adjust for covariates
+#' @param covariate_level A character string indicating whether covariates are assumed to affect
+#'   the underlying gene expression levels (\code{"z"}) or the observed counts (\code{"x"}).
+#'
+#' @return Design matrix (n by K) for moment-based regressions
+#'
+#'
+set_D <- function(s, D, adjust_setting, covariate_level){
+  if(!adjust_setting){
+    return(matrix(s, ncol = 1))
+  }else{
+    if(covariate_level == 'z'){
+      return(s * D)
+    }else if(covariate_level == 'x'){
+      return(cbind(s, D[,-1]))
+    }
+  }
+}
+
+#' Check for non-integer values in a matrix
+#'
+#' This function checks whether a numeric matrix contains any non-integer values.
+#'
+#' @param mat A numeric matrix.
+#'
+#' @return A logical value: \code{TRUE} if any element is non-integer, otherwise \code{FALSE}.
+#'
+#'
+has_non_integer <- function(mat) {
+  for (val in mat) {
+    if (abs(val - round(val)) > .Machine$double.eps^0.5) return(TRUE)
+  }
+  return(FALSE)
 }
---
+++ R/CSCORE_IRLS_base.R
@@ -0,0 +1,115 @@
+#' Original implementation Iteratively reweighted least squares (IRLS) procedure (archived)
+#'
+#' This function was originally implemented in 2023 and included in the first release of the CS-CORE R package.
+#' It has since been replaced by \code{\link{CSCORE_IRLS}} for two main reasons:
+#'
+#' \enumerate{
+#'   \item It relies on base R for regression, which is slower and more memory-intensive than the Rcpp-based implementation in \code{CSCORE_IRLS}.
+#'   \item It does not support covariate adjustment, a feature supported in \code{CSCORE_IRLS}.
+#' }
+#'
+#' @note This function is retained for reference and backward compatibility, but users are encouraged to use \code{\link{CSCORE_IRLS}} for new analyses.
+#'
+#' @keywords internal
+#'
+#' @param X A n by p matrix of UMI counts, where n denotes the number of cells and p denotes the number of genes
+#' @param seq_depth A length n vector of sequencing depths
+#' @param post_process Whether to process the estimated co-expressions such that the estimates are between -1 and 1. Default to TRUE.
+#'
+#' @return A list of three p by p matrices:
+#' \describe{
+#'   \item{est}{co-expression estimates}
+#'   \item{p_value}{p values}
+#'   \item{test_stat}{test statistics}
+#' }
+#' @export
+#'
+#' @examples
+#' ## Toy example:
+#' ## run CSCORE on a simulated independent gene pair
+#' cscore_example <- CSCORE_IRLS_base(ind_gene_pair$counts, ind_gene_pair$seq_depths)
+#'
+#' ## Estimated co-expression between two genes
+#' cscore_example$est[1,2]
+#' # close to 0: 0.007820124
+#'
+#' ## p-values
+#' cscore_example$p_value[1,2]
+#' # not significant: 0.961981
+#'
+#' @source Su, C., Xu, Z., Shan, X., Cai, B., Zhao, H., & Zhang, J. (2023).
+#' Cell-type-specific co-expression inference from single cell RNA-sequencing data.
+#' \emph{Nature Communications}.
+#' doi: <https://doi.org/10.1038/s41467-023-40503-7>
+#'
+CSCORE_IRLS_base <- function(X, seq_depth, post_process = TRUE){
+  if (is.null(seq_depth)) {
+    seq_depth = apply(X, 1, sum, na.rm = T)
+  }
+  if(nrow(X) != length(seq_depth)){
+    stop('The length of the sequencing depth must match the number of cells.')
+  }
+  n_cell = nrow(X)
+  n_gene = ncol(X)
+  seq_depth_sq = seq_depth^2
+  seq_2 = sum(seq_depth_sq)
+  seq_4 = sum(seq_depth^4)
+  mu = colSums(X * seq_depth)/seq_2
+  M = outer(seq_depth, mu)
+  X_centered = X - M
+  sigma2 = colSums(((X_centered^2 - M) * seq_depth_sq))/seq_4
+  theta = mu^2/sigma2
+  j = 0
+  delta = Inf
+
+  while( delta > 0.05 & j <= 10 ){
+    theta_previous = theta
+    theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
+    theta[theta < 0] = Inf
+    w = M + outer(seq_depth_sq, mu^2/theta_median)
+    w[is.na(w)|w <= 0] = 1
+    mu = colSums((X/w) * seq_depth)/colSums(seq_depth_sq/w)
+    M = outer(seq_depth, mu)
+    X_centered = X - M
+    h = (M^2/theta_median + M)^2
+    h[h <= 0] = 1
+    sigma2 = colSums(((X_centered^2 - M)/h * seq_depth_sq))/colSums(seq_depth_sq^2/h)
+    theta = mu^2/sigma2
+    j = j+1
+    # print(paste0("Iteration: ", j, ", Median: ", theta_median))
+    delta = max(abs(log((theta/theta_previous)[theta > 0 & theta_previous > 0])), na.rm = T)
+    # print(delta)
+  }
+  if(j == 10 & delta > 0.05){
+    print('IRLS failed to converge after 10 iterations. Please check your data.')
+  }else{
+    print(sprintf('IRLS converged after %i iterations.', j))
+  }
+
+  theta_median = stats::quantile(theta[theta > 0], na.rm = T, probs = 0.5)
+  theta[theta < 0] = Inf
+  w = M + outer(seq_depth_sq, mu^2/theta_median)
+  w[is.na(w)|w <= 0] = 1
+
+  covar = matrix(NA, nrow = n_gene, ncol = n_gene)
+  covar <- (t(seq_depth_sq * X_centered/w) %*% (X_centered/w))/(t(seq_depth_sq/w) %*% (seq_depth_sq/w))
+
+  # Evaluate test statistics and p values
+  Sigma <- M + outer(seq_depth_sq, sigma2)
+  ele_inv_Sigma <- 1/Sigma
+  X_centered_scaled <- X_centered * ele_inv_Sigma
+  num <- t(seq_depth_sq * X_centered_scaled) %*% X_centered_scaled
+  deno <- sqrt(t(seq_depth^4 * ele_inv_Sigma) %*% ele_inv_Sigma)
+  test_stat <- num/deno
+  p_value <- 2 * stats::pnorm(abs(test_stat), lower.tail = F)
+
+  # Evaluate co-expression estimates
+  neg_gene_inds <- which(sigma2 < 0)
+  sigma2[neg_gene_inds] <- 0
+  sigma <- sqrt(sigma2)
+  est <- covar/outer(sigma, sigma)
+
+  # Post-process the co-expression estimates
+  if(post_process) est <- post_process_est(est)
+  return(list(est = est, p_value = p_value, test_stat = test_stat))
+}
